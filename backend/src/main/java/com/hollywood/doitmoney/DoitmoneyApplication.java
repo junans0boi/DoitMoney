d64d8cfadf88da964c6348536b9e4e347568fbc6
Junhwan Lee <105141262+junans0boi@users.noreply.github.com>
2025-09-18 19:11:14 +0900

    feat: 레펙토링과 도커등 여러가지 이유로 새로운 프로젝트를 생성했음

---
+++ backend/src/main/java/com/hollywood/doitmoney/DoitmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.hollywood.doitmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoitmoneyApplication.class, args);
+	}
+
+}
